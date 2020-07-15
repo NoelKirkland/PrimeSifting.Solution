3f0c996bf74f77da7307b05eeed7bc1f76ff8c89
Erich Richter <erich.richter@gmail.com>
2020-07-15 14:01:18 -0700

    add initial files > > Co-authored-by: Kate Skorija <kateskorija@gmail.com> Co-authored-by: Noel Kirkland <noelkirkland@gmail.com>

---
+++ PrimeSifting.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+using System;
+
+namespace PrimeSifting.Tests
+{
+  [TestClass]
+
+  public class PrimeSiftingTest
+  {
+
+
+
+
+
+    
+  }
+
+  
+}
+// public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
---
+++ Program.cs
@@ -0,0 +1,19 @@
+using ToDoList.Models;
+using System;
+
+namespace PrimeSifting
+{
+
+  class Program
+  {
+
+    public static void Main()
+    {
+
+
+
+
+      
+    }
+  }
+}
